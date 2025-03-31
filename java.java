77f24c70353b3183dac6341cd010a25a5e6e57d9
AldenR389 <parsoncabin30@gmail.com>
2025-03-31 13:59:47 -0600

    Add files via upload

---
+++ java.java
@@ -0,0 +1,13 @@
+//public class helloword: public regulates access; makes it public, class defines a class, HelloWorld is the class definition.
+
+public class java {
+
+//Entry point for the command-line, handles arguments
+
+    public static void main(String[] args) {
+
+//Prints Hello,World!
+
+        System.out.println("Hello, World!");
+    }
+}
